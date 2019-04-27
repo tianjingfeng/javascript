8ba94de60915cfab463dfec9f72f21748fa89c3d
tianjingfeng <43663677+tianjingfeng@users.noreply.github.com>
2019-04-27 21:09:14 +0800

    l练习

---
+++ 练习/数据类型/a.js
@@ -0,0 +1,24 @@
+console.log(WhatEver);
+var WhatEver=document.getElementById('amao')
+console.log(WhatEver)
+console.log(1/0)
+console.log(0/100)
+
+
+if(undefined===null) {
+  document.write(typeof(undefined))
+} else {
+  document.write(typeof(null))
+}
+
+if(100==='100') {
+  document.write(typeof(100))
+} else {
+  document.write(typeof('100'))
+}
+
+if('1'===true) {
+  document.write(typeof(1))
+} else {
+  document.write(typeof('true'))
+}
